731070e4565a9e147e3934c685e41da331e4b068
nmajuterence <nmajuterence@gmail.com>
2024-07-11 00:23:15 +0100

    day 2 of python

---
+++ week_1/basics_python.py
@@ -0,0 +1,6 @@
+# to get an output from python you need to use a print() function
+print('lets see how print works')
+print('first', 'second', 'whatever', 2024, 30, True, False, ['papaya', 'mango', 'avocado'])
+# python has many build in functions example print(), len(), input(), round, etc
+print('i love the print function')
+print('hello, world!')
---
+++ week_1/buildin_functions.py
@@ -0,0 +1,22 @@
+# print(), len(), input(), round
+
+print('i love using print because it shows me what is happening')
+print('Nmaju')
+print(len('Nmaju'))
+print(len('eye ball'))
+print(['tomato','potato','milk'])
+
+# variables
+# a variable is a place to store data
+
+firstname = input('Enter your name: ')
+age = input('Enter your age: ')
+
+print(f'{firstname} you are {age} years old.')
+
+print(round(9.81))
+print(round(3.14))
+print(round(2.77))
+print(round(2.50))
+
+# abs(), int(), str(), sum(), min(), max(), type(), bool(), range(), id(), dir()

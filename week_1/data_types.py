44662b2ae7c0e6ac0431381d52d64c5c780289ea
nmajuterence <nmajuterence@gmail.com>
2024-07-12 23:45:57 +0100

    data types

---
+++ week_1/data_types.py
@@ -0,0 +1,35 @@
+print('we will learn data types')
+
+# number int, float, complex, string, boolean, list, tuple, set, dictionary
+print(10)
+print(type(10))
+print(9.81)
+print(type(9.81))
+print(1 + 5j)
+print(type(1 + 4j))
+
+# strings
+print('nmaju')
+print(type('nmaju'))
+print('i learned to do hard things the hard way!')
+print(len('i learned to do hard things, the hard way!'))
+print(len('nmaju'))
+
+# list - ordered, can be modified
+print([1, 2, 3, 4, 5])
+print(['nmaju', 'kiara', 'frida', 'enih', 'mau', 'noe'])
+print(['cameroon', 'nigeria', 'rwanda', 'ghanna', 'south africa'])
+
+# tuple
+print((1, 2, 3, 4, 5))
+print(type((1, 2, 3, 4, 5)))
+
+# set
+print({1, 2, 3, 4, 5})
+print(type({1, 2, 3, 4, 5}))
+print({'n', 'm', 'a', 'j', 'u'})
+print(type({'n', 'm', 'a', 'j', 'u'}))
+
+# dictionaries
+print({'firstname':'nmaju', 'lastname':'terence'})
+print(type({'firstname':'nmaju', 'lastname':'terence'}))

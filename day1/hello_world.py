97fe3c4fba2794a7bb77715d1419bcc954da03ed
nmajuterence <nmajuterence@gmail.com>
2024-07-12 01:13:11 +0100

    day 2

--- day1/hello_world.py
+++
@@ -1,3 +0,0 @@
-print('Hello World')
-print("Hello World")
-print(2024, 'I love people,', 'I live in Douala,', 'I am learning python.')
--- week_1/buildin_functions.py
+++ week_1/buildin_functions.py
@@ -8,11 +8,11 @@ print(['tomato','potato','milk'])
 
 # variables
 # a variable is a place to store data
-
+"""
 firstname = input('Enter your name: ')
 age = input('Enter your age: ')
 
-print(f'{firstname} you are {age} years old.')
+print(f'{firstname} you are {age} years old.')"""
 
 print(round(9.81))
 print(round(3.14))
@@ -20,3 +20,23 @@ print(round(2.77))
 print(round(2.50))
 
 # abs(), int(), str(), sum(), min(), max(), type(), bool(), range(), id(), dir()
+print(abs(-5))
+print(int('2') + 2)
+print('002' + str(2))
+print('hello' + ' ' + 'world')
+print(sum([1, 2, 3]))
+print(min(1, 3, 4, 10, -10))
+print(min([1, 3, 4, 10, -10]))
+print(max(1, 3, 4, 10, -10))
+print(max([1, 3, 4, 10, -10]))
+print(type('nmaju'))
+print(type(2024))
+print(type(9.81))
+print(type(1 + 1j))
+print(type([1, 2, 3]))
+print(type((1, 2, 3)))
+print(type({1, 2, 3}))
+print(type({'name':'nmaju'}))
+print(type(1 > 0))
+print(range(10))
+print(list([1, 3, 5, 7, 9]))

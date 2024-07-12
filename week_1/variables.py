d1d30020bbd98bc7018e14a3727e088b52472df5
nmajuterence <nmajuterence@gmail.com>
2024-07-13 00:33:43 +0100

    variables

---
+++ week_1/variables.py
@@ -0,0 +1,37 @@
+# variables - there are containers to store data.
+a = 4
+b = 3
+addition = a + b # 4 + 3 = 7
+subtraction = a - b # 4 - 3 = 1
+muiltiplication = a * b # 4 * 3 = 12
+division = a / b # 4 / 3 = 1.3333333333
+remainder = a % b # 4 % 3 = 1
+exponential = a ** b # 4 ** 3 = 64
+
+print('addition: ', addition)
+print('subraction: ', subtraction)
+print('multiplication: ', muiltiplication)
+print('division: ', division)
+print('remainder: ', remainder)
+print('exponential: ', exponential)
+
+first_name = 'terence'
+last_name = 'nmaju'
+date_of_birth = 1993
+current_year = 2024
+age = current_year - date_of_birth
+country = 'cameroon'
+city = 'douala'
+company = 'digitalxpress'
+website = 'https://digitalxpress.io'
+
+
+print('first name: ', first_name)
+print('last name: ', last_name)
+print('date of birth: ', date_of_birth)
+print('current year: ', current_year)
+print('age: ', age)
+print('country: ', country)
+print('city: ', city)
+print('company: ', company)
+print('website: ', website)

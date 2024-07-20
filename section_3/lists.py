18491173f42b6b8e37298ad306ecb5553de82455
nmajuterence <nmajuterence@gmail.com>
2024-07-20 23:31:46 +0100

    lists

---
+++ section_3/lists.py
@@ -0,0 +1,25 @@
+# list
+shoping_list = ['milk', 'meat', 'rice', 'beans', 'fish']
+print(shoping_list)
+print(len(shoping_list))
+
+numbers = [1, 2, 3, 4, 5, 6]
+regions_in_cameroon = ['north west', 'south west', 'litoral', 'centre', 'east', 'north']
+names = ['frida', 'kiara', 'nmaju', 'maureen', 'thelly']
+
+print(numbers[0])
+print(numbers[1])
+print(numbers[2])
+print(numbers[3])
+print(numbers[4])
+print(numbers[5])
+
+# get the last index of a list
+last_index = len(numbers) - 1
+print(last_index)
+
+# string slicing numbers[start:end:step]
+print(numbers[2:])
+print(numbers[1:5])
+print(numbers[::-1]) # reverses the list
+print(numbers[:3])

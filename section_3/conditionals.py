4efa4a3d280e9e45e77ba41ef17c3031250fe220
nmajuterence <nmajuterence@gmail.com>
2024-07-25 22:21:34 +0100

    conditionals

---
+++ section_3/conditionals.py
@@ -0,0 +1,10 @@
+# conditionals
+a = 2.5
+if a > 0:
+    print('a is a positive number')
+elif a < 0:
+    print('a is a negative number')
+elif a == 0:
+    print('a is zero')
+else:
+    print('a is not an integer')

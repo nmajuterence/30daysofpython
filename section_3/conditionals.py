c8ad07f542bcde56c2a3bdeda697b17f348c610e
nmajuterence <nmajuterence@gmail.com>
2024-07-26 23:32:11 +0100

    conditionals

--- section_3/conditionals.py
+++ section_3/conditionals.py
@@ -1,10 +1,16 @@
 # conditionals
-a = 2.5
-if a > 0:
-    print('a is a positive number')
-elif a < 0:
-    print('a is a negative number')
-elif a == 0:
-    print('a is zero')
+# if and else statements
+a = 0.5
+if type(a) == int:
+    if a > 0:
+        print('{} is a positive number'.format(a))
+    elif a < 0:
+        print('{} is a negative number'.format(a))
+    elif a == 0:
+        print('{} is zero'.format(a))
+    else:
+        print('{} is not a number'.format(a))
 else:
-    print('a is not an integer')
+    print('{} is a float'.format(a))
+
+# short hand

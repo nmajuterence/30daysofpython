0387f2f312b9fd0934f19d55137cdcdc1e640086
nmajuterence <nmajuterence@gmail.com>
2024-07-27 22:48:44 +0100

    conditionals

--- section_3/conditionals.py
+++ section_3/conditionals.py
@@ -13,7 +13,7 @@ if type(a) == int:
 else:
     print('{} is a float'.format(a))
 
-# exercise level 1 a
+"""# exercise level 1 a
 age = int(input('Enter your age: '))
 if age >= 18:
     print('you are old enough to learn to drive')
@@ -52,3 +52,54 @@ elif score >= 50 and score < 60:
     print('your grade is a "D"')
 else:
     print('your grade is an "F"')
+
+# exercise level 2 b
+month = input('enter a month to check the season: ')
+if month == 'september' or 'october' or 'november':
+    print('the season is autumn')
+elif month == 'december' or 'january' or 'february':
+    print('the season is winter')
+elif month == 'march' or 'april' or 'may':
+    print('the season is spring')
+elif month == 'june' or 'july' or 'august':
+    print('the season is summer')
+else:
+    print('please enter a real month or check your spelling!')"""
+    
+# exercise level 3a
+person={
+    'first_name': 'nmaju',
+    'last_name': 'terence',
+    'age': 30,
+    'country': 'cameroon',
+    'is_marred': True,
+    'skills': ['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
+    'address': {
+        'street': 'antenne orange',
+        'city': 'douala',
+        'zipcode': '237'
+    }
+}
+if person['skills']:
+    print(person['skills'][2])
+else:
+    print('the skills list is empty')
+
+# exercise level 3b
+if person['skills']:
+    if person['skills'][4] == 'Python':
+        print(person['skills'][4])
+    else:
+        print('Python is not one of the skills')
+else:
+    print('python is not in the dictionary')
+
+# exercise level 3c
+if 'JavaScript' in person['skills'] and 'React' in person['skills'] and len(person['skills']) == 2:
+    print('he is a front end developer')
+elif 'Node' in person['skills'] and 'Python' in person['skills'] and 'MongoDB' in person['skills']:
+   print('he is a back end developer')
+elif 'Node' in person['skills'] and 'React' in person['skills'] and 'MongoDB' in person['skills']:
+    print('he is a fullstack developer')
+else:
+    print('unknown title')

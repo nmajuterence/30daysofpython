3dbab5118d8148869702c93dd925ace0381bdfb2
nmajuterence <nmajuterence@gmail.com>
2024-07-31 21:59:22 +0100

    pynative exercise

--- section_3/pynative.py
+++ section_3/pynative.py
@@ -3,3 +3,12 @@ str1 = 'james'
 str2 = str1[0:5:2] # slice the string
 print(str2)
 
+# Exercise 2: Append new string in the middle of a given string
+s1 = 'Ault'
+s2 = 'Kelly'
+mid_s = len(s1) // 2 # the mid point
+start_s1 = s1[0:2]
+end_s1 = s1[2:]
+new_string = start_s1 + s2 + end_s1
+print(new_string)
+
